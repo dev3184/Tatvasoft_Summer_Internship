533ce9e5663d3367436ebd6eb3a7e6c67f2c2846
dev3184 <devbhalani3184@gmail.com>
2025-07-03 14:24:39 +0530

    Initial commit of student form app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { StudentFormComponent } from './student-form/student-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, StudentFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'student-form-app';
-}
+
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { StudentFormComponent } from './student-form/student-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   // default
+  // OR
+  { path: '', component: StudentFormComponent },
+];
